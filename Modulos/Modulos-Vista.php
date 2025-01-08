1cff81a119f57690f284ce22d6d9e380c7386a26
juanda <judagoncer@gmail.com>
2025-01-08 16:25:57 -0500

    cambié un scrpit que tenia en la vista aparte para que se vea más organizado

--- Modulos/Modulos-Vista.php
+++ Modulos/Modulos-Vista.php
@@ -134,31 +134,5 @@ $result = $conn->query($sql);
 include_once '../componentes/footer.php';
 ?>
 <script src="js/Datatables-Modulos.js"></script>
+<script src="js/Consultas-Modulos.js"></script>
 
-<script>
-    function crearModulo() {
-    // Validar el formulario antes de enviar
-    const form = document.getElementById('formModulo');
-    
-    if (form.checkValidity()) {
-        // Si el formulario es válido, proceder a enviar los datos
-        $('#modalModulos').modal('hide'); // Cierra el modal antes de enviar
-        // Aquí puedes agregar tu lógica para enviar el formulario, por ejemplo, usando AJAX
-        $.ajax({
-            url: 'Modulos-Controlador.php',
-            type: 'POST',
-            data: $(form).serialize(), // Serializa el formulario
-            success: function(response) {
-                // Manejar la respuesta del servidor
-                alert('Módulo creado exitosamente.');
-            },
-            error: function() {
-                alert('Hubo un error al crear el módulo.');
-            }
-        });
-    } else {
-        // Si el formulario no es válido, mostrar los mensajes de error
-        form.classList.add('was-validated');
-    }
-}
-</script>
