70b6ca9d659fdd498a692ccc6994fed77901ea65
Juankerk3k <jxxnfr70@gmail.com>
2025-01-24 15:25:39 -0500

    eliminacion de alertas innecesarias

--- Modulos/js/Consultas-Modulos.js
+++ Modulos/js/Consultas-Modulos.js
@@ -1,19 +1,13 @@
 function crearModulo() {
-    // Validar el formulario antes de enviar
     const form = document.getElementById('formModulo');
     
     if (form.checkValidity()) {
-        // Si el formulario es válido, proceder a enviar los datos
-        $('#modalModulos').modal('hide'); // Cierra el modal antes de enviar
-        // Aquí puedes agregar tu lógica para enviar el formulario, por ejemplo, usando AJAX
+        $('#modalModulos').modal('hide');
         $.ajax({
             url: 'Modulos-Controlador.php',
             type: 'POST',
-            data: $(form).serialize(), // Serializa el formulario
+            data: $(form).serialize(), 
             success: function(response) {
-                // Manejar la respuesta del servidor
-                alert('Módulo creado exitosamente.');
-                console.log('Recargando la página...');
                 location.reload();
             },
             error: function() {
@@ -21,7 +15,6 @@ function crearModulo() {
             }
         });
     } else {
-        // Si el formulario no es válido, mostrar los mensajes de error
         form.classList.add('was-validated');
     }
 }
